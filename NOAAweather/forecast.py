8bd6d3feca00c69fad6cbef682a91c2e685c1aef
Daniel Hill <dhill90@gmail.com>
2015-02-15 22:47:53 -0500

    Forecast (single and multi) return dictionaries.
    Forecast.single_point and Forecast.multi_point now return dictionaries
    consisting of only the parameters of the returned XML (only the
    information from the elements list)

--- NOAAweather/forecast.py
+++ NOAAweather/forecast.py
@@ -1,14 +1,9 @@
 try:
-    import urllib2
-    assert urllib2
-    import xml
-    assert xml
+    import urllib.request as request
     import xmltodict
     assert xmltodict
     import datetime
     assert datetime
-    import Enum
-    assert Enum
 except ImportError:
     pass
 
@@ -28,7 +23,7 @@ class Forecast(object):
     def __point_url_construction__(self,list_lat_lon,begin,end,elements):
         listLatLon = "listLatLon={},{}".format(str(list_lat_lon[0][0]), str(list_lat_lon[0][1]))
         for lat,lon in list_lat_lon[1:]:
-            listLatLon = "{} {},{}".format(listLatLong, str(lat), str(lon))
+            listLatLon = "{} {},{}".format(listLatLon, str(lat), str(lon))
         product = "&product={}".format(self.prod)
         times = "&begin={}&end={}".format(begin, end)
         unit = "&Unit={}".format(self.unt)
@@ -38,11 +33,17 @@ class Forecast(object):
             element_string = "{}{}".format(element_string,new_element)
         return self.url + listLatLon + product + times + unit + element_string
 
+
     # @params: list [[lat, lon]], begin, end, NDFD elements
     # @return: a dictionary with information about the NDFD elements
     def single_point(self,list_lat_lon,begin,end,elements):
         single_url = self.__point_url_construction__(list_lat_lon,begin,end,elements)
-        return single_url
+        with request.urlopen(single_url) as noaa:
+            noaa_dict = xmltodict.parse(noaa.read())
+        params = noaa_dict['dwml']['data']['parameters']
+        print (params)
+        return #params
+
 
     # @params: list [[lat, lon],[lat,lon]] product, begin, end, unit, NDFD elements
     # @return: a dictionary with information about the NDFD elements
@@ -86,5 +87,8 @@ class PointFinder(object):
     def corners_grid():
         pass
 
+
+
 obj = Forecast()
+#print(obj.__point_url_construction__([(37.2988451,-78.40321589999999)], '','',['maxt','mint','pop12']))
 print (obj.single_point([(37.2988451,-78.40321589999999)], '','',['maxt','mint','pop12']))
